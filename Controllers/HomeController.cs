5b9b5f58d69f1566be2a8c41f4304ef11c4bb381
Vitaly Abrosimov <vetalman23@gmail.com>
2023-05-01 00:17:51 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using module_19.Models;
+using System.Diagnostics;
+
+namespace module_19.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail1()
+        {
+            return View();
+        }
+
+        public IActionResult Detail2()
+        {
+            return View();
+        }
+        public IActionResult Detail3()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/TxtParse.cs
@@ -0,0 +1,19 @@
+﻿namespace module_19.Models
+{
+    public class TxtParse
+    {
+        public static string ArrayPerson(int a, int b)
+        {
+            string[] lines = File.ReadAllLines("PhoneBook.txt");
+            string[,] phoneBook = new string[lines.Length, lines[0].Split(' ').Length];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] pbTemp = lines[i].Split('#');
+                for (int j = 0; j < pbTemp.Length; j++)
+                    phoneBook[i, j] = pbTemp[j];
+            }
+            return phoneBook[a,b];
+        }
+
+    }
+}
